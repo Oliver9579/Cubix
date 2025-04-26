73c9320452e38a3ce2c1851b55dab18769b96714
Oliver9579 <szabo.oliver2001@gmail.com>
2025-04-26 18:15:17 +0200

    angular-counter is changed

--- Angular/W1/angular-counter/src/app/app.component.ts
+++ Angular/W1/angular-counter/src/app/app.component.ts
@@ -1,23 +1,45 @@
 import {Component} from '@angular/core';
-import {CounterComponent} from './counter/counter.component';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {CounterComponent} from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, CounterComponent],
+  imports: [CommonModule, FormsModule, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'First Angular app'
   logoSrc = "https://upload.wikimedia.org/wikipedia/commons/c/cf/Angular_full_color_logo.svg"
-  counter = {name: 'Dishes', id: 1}
+  counters: { name: string, id: number, aim: number }[] = [];
   aim = 5;
+  isCompleted = false;
+  counterCount = 0;
 
   countChanged(count: number) {
     if (count === this.aim) {
+      this.isCompleted = true;
       alert('Goal reached')
     }
 
+    if (count === 3) {
+      this.aim = 10;
+      // this.counter = {name: 'Clothes', id: 2}
+    }
+  }
+
+  counterCountChanged() {
+    const diff = this.counterCount - this.counters.length;
+    if (diff > 0) {
+      const newCounters = new Array(diff).fill(undefined).map((_, i) => ({
+        name: '',
+        id: i + this.counters.length,
+        aim: 10
+      }))
+      this.counters = [...this.counters, ...newCounters]
+    } else if (diff < 0) {
+      this.counters.splice(this.counterCount);
+    }
   }
 }
--- Angular/W1/angular-counter/src/app/counter/counter.component.ts
+++ Angular/W1/angular-counter/src/app/counter/counter.component.ts
@@ -1,24 +1,40 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-counter',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
-  @Input() aim = 10;
+  @Input() aim = 5;
 
   @Input({required: false}) counter?: { name: string, id: number }
 
   @Output() countChange = new EventEmitter<number>();
 
+  @ViewChild('btn') buttonRef?: ElementRef<HTMLButtonElement>;
+
   count = 0;
+  changeDetectionCounter = 0;
+
+  constructor() {
+  }
 
   incrementCounter() {
     this.count = this.count + 1;
     this.countChange.emit(this.count);
   }
 
+  get log() {
+    this.changeDetectionCounter++;
+    // console.log(this.changeDetectionCounter);
+    return 'change detection test'
+  }
+
+  get isComplete() {
+    return this.count >= this.aim;
+  }
 }
